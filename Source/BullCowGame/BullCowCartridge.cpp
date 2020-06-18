7e819005fe43de38dee77ea1a807978114420add
gcconantc <gcconantc@gmail.com>
2020-06-18 23:59:24 +0200

    change map, add some text and use TEXT macro

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Hello, welcome to bull cows"));
+    PrintLine(TEXT("press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    FString HiddenWord = TEXT("secret");
 }
