2a3c1b67e1782d0d090c4b3aa709dc13bd265a96
gcconantc <gcconantc@gmail.com>
2020-06-20 17:56:43 +0200

    Refacto

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -16,30 +16,19 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     }
     else 
     {
-        if (Input == HiddenWord)   
-        {
-            PrintLine(TEXT("you have won !"));
-            EndGame();
-        }
-        else
-        {
-            if (Input.Len() != HiddenWord.Len()) 
-            {
-                PrintLine(TEXT("The hidden word is %i letters long."), HiddenWord.Len());
-                EndGame();
-            }
-        }
+        ProcessGuess(Input, Lives);
     }
 }
 
 void UBullCowCartridge::SetupGame()
-{
-    Lives = 5;
+{    
     HiddenWord = TEXT("secret");
+    Lives = HiddenWord.Len();
     bGameOver = false;
 
     PrintLine(TEXT("Hello, welcome to bull cows"));
     PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives"), Lives);
     PrintLine(TEXT("Type in your guess and press enter to continue..."));
 }
 
@@ -47,4 +36,30 @@ void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
     PrintLine(TEXT("Press enter to play again."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess, int32 counter)
+{
+  if (Guess == HiddenWord)   
+        {
+            PrintLine(TEXT("you have won !"));
+            EndGame();
+        }
+        else
+        {
+            --counter;
+
+            if (counter < 0) 
+            {
+                PrintLine(TEXT("No lives left. You lost."), counter);
+                EndGame();
+            }
+            else 
+            {
+                if (Guess.Len() != HiddenWord.Len()) 
+                {
+                   PrintLine(TEXT("Try guessing again. You have %i lives"), counter);
+                }
+            }
+        }
 }
